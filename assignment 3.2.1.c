b3d5f5bce6ae9e37157e2b13591d9bb6056d04c0
sneha6201 <snehakumari70024@gmail.com>
2025-11-12 14:19:06 +0530

    Add files via upload

---
+++ assignment 3.2.1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+
+    printf("enter a value of a\t");
+    scanf("d%",&a);
+
+    printf("enter a value of b\t");
+    scanf("d%",&b);
+
+    printf("enter a value of c\t");
+    scanf("d%",&c);
+
+     D=b^2-4*a*c;
+
+    if(D>0){
+            printf("real & distance");
+
+
